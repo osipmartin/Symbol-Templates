547d3813446ee436d33db956a3aa612bfdd8f6fd
osipmartin <pmartin@osisoft.com>
2017-08-31 11:11:54 -0400

    Initial Commit

---
+++ ToolpaneTemplate.js
@@ -0,0 +1,16 @@
+(function (PV){
+	"use strict";
+
+	var def = {
+		typeName: "", 
+		inject: ['$rootScope'],
+		init: init 
+	} 
+
+	function init($rootScope) {
+
+	}
+
+	PV.toolCatalog.register(def); 
+
+})(window.PIVisualization)
